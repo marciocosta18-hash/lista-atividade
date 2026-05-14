7cf89c24cd18c7ef96baae49ba5c4a0dfe0e29c1
marciocosta18-hash <marcio.costa18@hotmail.com>
2026-05-14 19:35:33 -0300

    Update lista_exercicio.py

--- lista_exercicio.py
+++ lista_exercicio.py
@@ -1,7 +1,8 @@
-print("-" *40)
+print("=" *40)
 print("Hello, World!")
 
 
 frutas = ["maçã", "banana", "laranja", "uva"]
+print("=" *40)
 print(frutas[1])  # Imprime "banana"
-print("-" *40)
+print("=" *40)
