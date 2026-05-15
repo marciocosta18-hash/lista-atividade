b4896394de3f6e2d58a10268a65220e4ea766199
marciocosta18-hash <marcio.costa18@hotmail.com>
2026-05-15 08:04:35 -0300

    Update lista_exercicio.py

--- lista_exercicio.py
+++ lista_exercicio.py
@@ -6,3 +6,20 @@ frutas = ["maçã", "banana", "laranja", "uva"]
 print("=" *40)
 print(frutas[1])  # Imprime "banana"
 print("=" *40)
+
+numeros = [1, 2, 3, 4, 5]
+print("=" *40)
+print(numeros[2])  # Imprime 3
+print("=" *40)
+animais = ["cachorro", "gato", "coelho", "pássaro"]
+print("=" *40)
+print(animais[0])  # Imprime "cachorro"
+print("=" *40)
+cores = ["vermelho", "azul", "verde", "amarelo"]
+print("=" *40)
+print(cores[3])  # Imprime "amarelo"
+print("=" *40)
+paises = ["Brasil", "Argentina", "Estados Unidos", "França"]
+print("=" *40)
+print(paises[2])  # Imprime "Estados Unidos"
+print("=" *40)
